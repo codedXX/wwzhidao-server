98d6f9cf57188722b22317083b1e7eda89de43a9
codeYx <2584912193@qq.com>
2026-01-30 18:10:54 +0800

    修改

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,37 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 
+/**
+ * 注入的意思是！！！！
+ * 1.注入 = 别自己 new，对象由框架帮你创建并塞给你用。 *
+ */
+
 @Module({
   imports: [
     ConfigModule.forRoot({
+      /**
+       * 不加isGlobal: true的话，imports: [ConfigModule]每个模块都要写一次，
+       * 加了后全局可用，自动注入
+       */
       isGlobal: true, //全局模块，可以在任何地方使用
     }),
+    /**
+     * 异步方式连接 MongoDB 数据库
+     */
     MongooseModule.forRootAsync({
-      imports: [ConfigModule],
+      imports: [ConfigModule], //把 ConfigModule“拉进”这个动态模块的作用域里，让它的服务（ConfigService）能被注入使用。
+      /**
+       * useFactory:这是一个工厂函数，专门用来生成数据库配置。
+       * 意思是：
+       * 1.先从 .env 读取 MONGODB_URI
+       * 2.如果没有，就用默认本地数据库
+       */
       useFactory: (configService: ConfigService) => ({
         uri:
           configService.get<string>('MONGODB_URI') ||
           'mongodb://localhost:27017/wwzhidao',
       }),
-      inject: [ConfigService],
+      inject: [ConfigService], //告诉Nest:这个工厂函数需要 ConfigService，帮我注入
     }),
     UserModule,
   ],
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  ParseIntPipe,
+  NotFoundException,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import type { User } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  /**
+   *   constructor(private readonly userService: UserService) {}  等价于
+   *  private readonly userService:UserService
+   *    constructor(userService: UserService) {
+   *        this.userService = userService;
+   *  }
+   *
+   */
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  findAll(): User[] {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number): User {
+    const user = this.userService.findOne(id);
+    if (!user) {
+      throw new NotFoundException(`用户 ID ${id} 不存在`);
+    }
+    return user;
+  }
+
+  @Post()
+  create(@Body() createUserDto: { name: string; email: string }): User {
+    return this.userService.create(createUserDto);
+  }
+}
